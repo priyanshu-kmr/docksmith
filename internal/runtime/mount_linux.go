fbc89812b7313e2a09f1bc5bd432b3f0c9791382
Raunak Bagaria <146432593+raunak-bagaria@users.noreply.github.com>
2026-04-07 20:25:52 +0530

    fix ordering

--- internal/runtime/mount_linux.go
+++
@@ -1,8 +0,0 @@
-package runtime
-
-import "syscall"
-
-// unixUnmount is a helper that performs a lazy unmount.
-func unixUnmount(target string) error {
-	return syscall.Unmount(target, syscall.MNT_DETACH)
-}
